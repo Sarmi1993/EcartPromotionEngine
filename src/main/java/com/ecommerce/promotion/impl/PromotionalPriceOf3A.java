36dd96e389fd2e6d16ef64dd5a2af4f3391e4bfa
Sarmi1993 <pritha003@gmail.com>
2020-09-21 17:32:11 +0530

    Adding the implementation of 3A calculation

--- src/main/java/com/ecommerce/promotion/impl/PromotionalPriceOf3A.java
+++ src/main/java/com/ecommerce/promotion/impl/PromotionalPriceOf3A.java
@@ -4,7 +4,9 @@ package com.ecommerce.promotion.impl;
 
 import java.util.HashMap;
 
+import com.ecommerce.bo.PromotionalpriceDetails;
 import com.ecommerce.promotion.IPromotionalPrice;
+import static com.ecommerce.bo.ProductIndividualPriceDetails.*;
 
 public class PromotionalPriceOf3A implements IPromotionalPrice {
    
@@ -13,6 +15,14 @@ public class PromotionalPriceOf3A implements IPromotionalPrice {
      **/
     @Override
     public long promotionalPrice(HashMap<String, Integer> checkoutProductList) {
-        return (Long) null;
+    	 int quantityOfA = 0;
+    	    
+         if(checkoutProductList.get("A")!=null) {
+         	quantityOfA=checkoutProductList.get("A");
+         }
+         int PromotionalCountOfA=quantityOfA/3;
+         int WithoutPromotionalCountOfB=quantityOfA-PromotionalCountOfA*3;
+         return PromotionalCountOfA * PromotionalpriceDetails.priceOf3A + WithoutPromotionalCountOfB * individualpriceList.get("A");
+        
     }
 }
